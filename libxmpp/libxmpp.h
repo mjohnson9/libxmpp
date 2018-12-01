da60509a69aeaed04b6c96e6d6c4641e24613a8e
Michael Johnson <michael@johnson.computer>
2018-12-01 10:21:59 -0500

    Initial Commit

---
+++ libxmpp/libxmpp.h
@@ -0,0 +1,19 @@
+//
+//  libxmpp.h
+//  libxmpp
+//
+//  Created by Michael Johnson on 12/1/18.
+//  Copyright © 2018 Michael Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libxmpp.
+FOUNDATION_EXPORT double libxmppVersionNumber;
+
+//! Project version string for libxmpp.
+FOUNDATION_EXPORT const unsigned char libxmppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libxmpp/PublicHeader.h>
+
+
